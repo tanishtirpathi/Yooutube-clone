a22b0048f1b39d5ea897a6438d79a89ab49c772e
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-04-24 15:00:40 +0530

    mini change to the variable

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,8 @@ import DB_NAME from "../constants.js";
 
 const connectDB = async()=>{
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/ ${DB_NAME}`)
-        console.log(`\n MongoDB is connected ya bro you did it !! DB host is running :${connection.connection.host}`)
+        const connectionIN = await mongoose.connect(`${process.env.MONGODB_URL}/ ${DB_NAME}`)
+        console.log(`\n MongoDB is connected ya bro you did it !! DB host is running :${connectionIN.connection.host}`)
     } catch (error) {
         console.log('error is :' , error)
         process.exit(1)
